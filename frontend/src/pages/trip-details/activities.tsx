3ce200c67de30ad1153940406eac83648d7785e0
Cairo <cairocampos98@gmail.com>
2024-07-12 16:00:27 -0300

    backend and frontend

---
+++ frontend/src/pages/trip-details/activities.tsx
@@ -0,0 +1,64 @@
+import { CircleCheck } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { format } from "date-fns";
+import { ptBR } from 'date-fns/locale'
+
+interface Activity {
+  date: string;
+  activities: {
+    id: string;
+    title: string;
+    occurs_at: string;
+  }[]
+}
+
+export function Activities() {
+  const { tripId } = useParams();
+
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+
+  const formatDate = (date: string, fmt: string) => {
+    return format(date,fmt, {
+        locale: ptBR
+    })
+  }
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/activities`)
+      .then((response) => setActivities(response.data.activities));
+  }, [tripId]);
+
+  return (
+    <div className="space-y-8">
+      {activities.map((category) => (
+        <div className="space-y-2.5">
+            <div className="flex items-baseline gap-2">
+                <span className="text-xl text-zinc-300 font-semibold">Dia {formatDate(category.date, 'd')}</span>
+                <span className="text-xs text-zinc-500">{formatDate(category.date, 'EE')}</span>
+            </div>
+
+            {category.activities.length ? (
+                <div className="space-y-2.5">
+                    {category.activities.map(activity => (
+                        <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                            <CircleCheck className="size-5 text-lime-300" />
+                            <span className="text-zinc-100">{activity.title}</span>
+                            <span className="text-zinc-400 ml-auto">{format(activity.occurs_at, "HH:mm'h'")}</span>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p className="text-zinc-500 text-sm">
+                    Nenhuma atividade cadastrada nessa data.
+                </p>
+            )}
+            
+        </div>
+      ))}
+    </div>
+  );
+}
